9baf94ff0daa3db71dc77698d0f6729c1f0d6361
mijimenezl <167145289+mijimenezl@users.noreply.github.com>
2024-10-02 08:56:20 -0500

    Update app.py

--- app.py
+++ app.py
@@ -47,9 +47,6 @@ remove_files(7)
 st.title("Reconocimiento Óptico de Caracteres")
 st.subheader("Elige la fuente de la imágen, esta puede venir de la cámara o cargando un archivo")
 
-image = Image.open('imgtranrec.jpg')
-
-st.image(image, width=500)
 
 
 cam_ = st.checkbox("Usar Cámara")
